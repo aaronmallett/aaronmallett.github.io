25ec2eed2d637b3ba25e02cf59f231c6da294714
Xilas <aaron_mallett@Aaron-Mallett.local>
2015-05-13 00:06:41 -0500

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,37 @@
+$(window).ready(function(){
+
+	$('.full-page-panel').height($(window).height());
+})
+
+
+$(window).resize(function() {
+
+	$('.full-page-panel').height($(window).height());
+
+});
+
+
+$(window).scroll(function(){
+
+	var wScroll = $(this).scrollTop();
+
+	$('.logo').css({
+		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+	});
+
+	$('.ruler').css({
+
+		'transform' : 'translate(0px, '+ wScroll /7.5 +'%)'
+
+	});
+
+	$('.pencil').css({
+
+		'transform' : 'translate(0px, -'+ wScroll /12 +'%)'
+
+	});
+
+
+
+});
+
